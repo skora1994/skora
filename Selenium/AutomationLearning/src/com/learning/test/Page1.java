cb71f4e04f77da54f3d9bb36bb3374e8d6ff7fd8
skora2019 <korasrvn@gmail.com>
2019-05-13 23:58:09 +0530

    Test

--- Selenium/AutomationLearning/src/com/learning/test/Page1.java
+++ Selenium/AutomationLearning/src/com/learning/test/Page1.java
@@ -5,7 +5,11 @@ public class Page1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testing");
-
+//Adding comment
+		for(int i=1;i<=10;i++)
+		{
+			
+		}
 	}
 
 }
